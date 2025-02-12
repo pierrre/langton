bde88955bfad6dbf13c92fe78fb911887d8a5982
Pierre Durand <pierredurand@gmail.com>
2025-02-12 18:21:07 +0100

    update benchmarks

--- langton_benchmark_test.go
+++ langton_benchmark_test.go
@@ -6,63 +6,63 @@ import (
 
 func BenchmarkGridGetInc(b *testing.B) {
 	g := NewGrid(Pt(10, 10), 2)
-	for range b.N {
+	for b.Loop() {
 		g.GetInc(Pt(5, 5))
 	}
 }
 
 func BenchmarkOrientationRotateRight1(b *testing.B) {
 	o := OrientationUp
-	for range b.N {
+	for b.Loop() {
 		o = o.Rotate(1)
 	}
 }
 
 func BenchmarkOrientationRotateRight5(b *testing.B) {
 	o := OrientationUp
-	for range b.N {
+	for b.Loop() {
 		o = o.Rotate(5)
 	}
 }
 
 func BenchmarkOrientationRotateLeft1(b *testing.B) {
 	o := OrientationUp
-	for range b.N {
+	for b.Loop() {
 		o = o.Rotate(-1)
 	}
 }
 
 func BenchmarkOrientationRotateLeft5(b *testing.B) {
 	o := OrientationUp
-	for range b.N {
+	for b.Loop() {
 		o = o.Rotate(-5)
 	}
 }
 
 func BenchmarkAntMoveUp(b *testing.B) {
 	a := &Ant{Orientation: OrientationUp}
-	for range b.N {
+	for b.Loop() {
 		a.Move(1)
 	}
 }
 
 func BenchmarkAntMoveRight(b *testing.B) {
 	a := &Ant{Orientation: OrientationRight}
-	for range b.N {
+	for b.Loop() {
 		a.Move(1)
 	}
 }
 
 func BenchmarkAntMoveDown(b *testing.B) {
 	a := &Ant{Orientation: OrientationDown}
-	for range b.N {
+	for b.Loop() {
 		a.Move(1)
 	}
 }
 
 func BenchmarkAntMoveLeft(b *testing.B) {
 	a := &Ant{Orientation: OrientationLeft}
-	for range b.N {
+	for b.Loop() {
 		a.Move(1)
 	}
 }
@@ -76,8 +76,7 @@ func BenchmarkGameStep(b *testing.B) {
 			Orientation: OrientationUp,
 		}},
 	}
-	b.ResetTimer()
-	for range b.N {
+	for b.Loop() {
 		g.Step()
 	}
 }
